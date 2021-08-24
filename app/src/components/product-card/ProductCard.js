144f558f9dedecef5fb7cd043f9a17b37942009c
jess-stack <72085164+jess-stack@users.noreply.github.com>
2021-08-24 23:02:39 +0200

    added price and rating

--- app/src/components/product-card/ProductCard.js
+++ app/src/components/product-card/ProductCard.js
@@ -10,7 +10,17 @@ export const ProductCard = ({ product }) => {
         className='product-image' 
         src={getProductImageLink(product)} 
         alt={product.id} />
-      <h3 className='product-title'>{product.name}</h3>
+      <h3 className='product-card__title'>{product.name}</h3>
+      <p className='product-card__description'>{product.description}</p>
+      <div className='price-rating-container'>
+        <p className='price-rating-container__price'>
+          {product.cheapestPrice.amount} kr
+        </p>
+        <p className='price-rating-container__rating'>
+          <i className='fa fa-star'></i>
+          {product.rating.averageRating}
+        </p>
+      </div>
     </div>
   )
 };
