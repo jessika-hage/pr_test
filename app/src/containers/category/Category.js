6d2e1de843c5dbe4eb86a0dd02f5413d86cd73d5
jess-stack <72085164+jess-stack@users.noreply.github.com>
2021-08-25 12:12:32 +0200

    added sorting functions and some styling on filter

--- app/src/containers/category/Category.js
+++ app/src/containers/category/Category.js
@@ -8,29 +8,46 @@ import { Filter } from '../../components/filter-bar/Filter';
 export const Category = () => {
   const [categoryData, setCategoryData] = useState([]);
   const [filterData, setFilterData] = useState(categoryData);
+  const [sortingData, setSortingData] = useState(filterData);
   const [value, setValue] = useState('');
 
   useEffect(() => {
     const loadedData = getCategoryList();
     setCategoryData(loadedData.products);
     handleFilter();
+    handleSorting();
   }, [value, categoryData]);
 
   const handleFilter = () => {
     setFilterData(categoryData.filter((category) => 
-    category.name.includes(value)))
+    category.name.includes(value)));
+  };
+
+  const handleSorting = (type) => {
+    setSortingData(filterData.sort((a, b) => {
+      if (type === 'price') {
+        return a.cheapestPrice.amount - b.cheapestPrice.amount;
+      } else if (type === 'rating') {
+          return b.rating.averageRating - a.rating.averageRating;
+        } else {
+            return b.numberOfMerchants - a.numberOfMerchants;
+        }
+    }))
   };
 
   return (
-    <>
+    <div className='product-container'>
       <Filter 
         brand={value} 
-        onBrandChange={(e) => setValue(e.target.value)} />
+        onBrandChange={(e) => setValue(e.target.value)}
+        onSortPrice={() => handleSorting('price')}
+        onSortRating={() => handleSorting('rating')}
+        onSortPopular={() => handleSorting('popular')} />
       <main className='main-container'>
         {filterData.map((item) => (
           <ProductCard key={item.id} product={item} />
         ))}
       </main>
-    </>
+    </div>
   )
 };
